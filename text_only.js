74f830c4298d6b993b61fab4daf7e533eafb623e
Chris Greene <archgoon@gmail.com>
2011-07-12 00:51:00 -0700

    Okay, hiding now works.

--- text_only.js
+++ text_only.js
@@ -1,17 +1,62 @@
-function hidetag(tags){
-	//alert("hi!"+tags);
-	for(tag in tags){
-		if(tags[tag] == "")
-		{
-		}
-		else{
-			var selection = $("div[id*=update]");
-			console.log(selection)
-			console.log(selection.filter(":contains("+tag+")"))
-			selection.filter(":contains("+tag+")")
-			.css("text-decoration","underline");
+function descend(start,childlist){
+	var curchild = start;
+	for(index in childlist) {
+		var i = childlist[index];
+		curchild = curchild.childNodes[i];
+	}
+	return curchild;
+}
+
+function tryToGetPostText(post){
+	try{
+		var domchild = post.children()[0];
+		var postText = descend(domchild,[1,0,0,0]).innerHTML;
+	}catch(err){
+		postText = "";
+		//console.log("caught err:"+err);
+	}
+	return postText;
+}
+
+function inList(value,alist){
+	console.log(value+":"+alist)
+	for(var index in alist){
+		if(value == alist[index]){
+			return true;
 		}
 	}
+	return false;
+}
+
+function hideTaggedPost(post,tag,hiddenlist){
+	var postText = tryToGetPostText(post);
+	if(postText.match(""+tag)){
+		console.log("tag:"+tag+"//text:"+postText+
+		            post.get(0));
+		hiddenlist[post.get(0)]=true;
+		post.css("display","none");
+	}
+}
+
+
+function hideTaggedPosts(tags){
+	var hiddenlist = {};
+	var selection = $("div[id*=update]");
+	for(var index in tags){
+		var tag = tags[index];
+		if(tag != ""){
+			selection.each(function (){
+				var $post = $(this);
+				hideTaggedPost($post,tag,hiddenlist);
+			});
+		}
+	}
+	selection.each( function(){
+		   var $post = $(this);
+		   if(hiddenlist[$post.get(0)] == undefined){
+			   $post.css("display","block");
+		   }
+		});
 }
 
 function gettags(){
@@ -19,7 +64,6 @@ function gettags(){
 	             filter(function(){return this.id.match(/\:..\.f/)});
 	return match.text().split(" ");
 }
-setInterval(function(){hidetag(gettags())},2000);
-hidetag(gettags())
+setInterval(function(){hideTaggedPosts(gettags());},1000);
 
 
