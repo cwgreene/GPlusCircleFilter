1b2bb614eb4e1e75010e12f953b5199d904455b9
Chris Greene <archgoon@gmail.com>
2011-07-12 22:49:21 -0700

    Updated filter_circle, now handles the situation where a circle does not start with a hash tag, but was in a weird loading state, so it kept the wrong circle tag, cleared everyone, and then never showed them later (since we had the "" as the circle hashtag).

--- filter_circle.js
+++ filter_circle.js
@@ -101,6 +101,7 @@ function getCircleHashTag(){
 
 function filter_circles(e){
 	var circle_hashtag=getCircleHashTag();
+	console.log(circle_hashtag);
 	var nofollow= "[^a-zA-Z0-9]";
 	var hashtag_regex = circle_hashtag+nofollow;
 	//console.log(circle_hashtag);
@@ -126,6 +127,10 @@ function filter_circles(e){
 				console.log(err);
 			}
 		});
+	}else{
+		//Show all
+		var selection = $("div[id*=update]");
+		selection.css("display","block");
 	}
 }
 
